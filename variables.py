380dc077215a206b24da9a1ad1224dfeab2044c9
krloos97 <krloos97@gmail.com>
2025-06-14 23:23:06 +0000

    actualizacion de variables()

--- variables.py
+++ variables.py
@@ -1,9 +1,9 @@
 # Simulación de usuarios
 global usuarios
 usuarios = [
-    {"usuario": "Dolo", "nombre": "Dolores_Fernandez", "contrasena": "1234", "rol": "estandar"},
-    {"usuario": "Fran", "nombre": "Franco_Fernandez", "contrasena": "1234", "rol": "estandar"},
-    {"usuario": "Lore", "nombre": "Lorena_Figueroa", "contrasena": "1234", "rol": "estandar"},
+    {"usuario": "Dolo", "nombre": "Dolores_Fernandez", "contrasena": "1234", "rol": "Usuario"},
+    {"usuario": "Fran", "nombre": "Franco_Fernandez", "contrasena": "1234", "rol": "Usuario"},
+    {"usuario": "Lore", "nombre": "Lorena_Figueroa", "contrasena": "1234", "rol": "Usuario"},
     {"usuario": "Mati", "nombre": "Matias_Rodriguez", "contrasena": "1234", "rol": "Administrador"},
     {"usuario": "Ale", "nombre": "Ale_Gariglio", "contrasena": "1234", "rol": "Administrador"}
 ]
